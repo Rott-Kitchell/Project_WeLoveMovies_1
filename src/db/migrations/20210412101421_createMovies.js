9e3034960cf4c6f77ace51aa61847c634dfd2f7f
RottK <r.kitchell815@gmail.com>
2021-04-12 14:04:08 -0500

    migration never used

--- src/db/migrations/20210412101421_createMovies.js
+++ src/db/migrations/20210412101421_createMovies.js
@@ -4,7 +4,7 @@ exports.up = function (knex) {
     table.string("title");
     table.integer("runtime_in_minutes");
     table.string("rating");
-    table.string("description");
+    table.string("description", 500);
     table.string("image_url");
     table.timestamps(true, true); // adds created_at and updated_at columns; passing true as the first argument sets the columns to be a timestamp type while passing true as the second argument sets those columns to be non-nullable and to use the current timestamp by default
   });
--- src/errors/asyncErrorBoundary.js
+++ src/errors/asyncErrorBoundary.js
@@ -1,4 +1,4 @@
-function asyncErrorBoundary(delegate, defaultStatus ) {
+function asyncErrorBoundary(delegate, defaultStatus) {
   return (request, response, next) => {
     Promise.resolve()
       .then(() => delegate(request, response, next))
@@ -12,4 +12,4 @@ function asyncErrorBoundary(delegate, defaultStatus ) {
   };
 }
 
-module.exports = asyncErrorBoundary
+module.exports = asyncErrorBoundary;
