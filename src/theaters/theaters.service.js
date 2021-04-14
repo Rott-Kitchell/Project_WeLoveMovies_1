a5aaf77bc85c70654c5a262d2ddce03495bede78
RottK <r.kitchell815@gmail.com>
2021-04-14 16:09:22 -0500

    passing all tests

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -1,6 +1,12 @@
 const knex = require("../db/connection");
 const mapProperties = require("../utils/mapProperties");
 
+async function listMoviesByTheater(theater_id) {
+  return await knex("movies as m")
+    .join("movies_theaters as mt", "m.movie_id", "mt.movie_id")
+    .where({ "mt.theater_id": theater_id });
+}
+
 function listTheaterByMovie(movie_id) {
   return knex("theaters as t")
     .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
@@ -10,10 +16,21 @@ function listTheaterByMovie(movie_id) {
 
 function listAllTheaters() {
   return knex("theaters as t")
-    .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
-    .join("movies as m", "m.movie_id", "mt.movie_id")
-    .select("t.*", "m.*")
-    .orderBy("t.name");
+    .orderBy("t.name")
+    .then((data) =>
+      Promise.all(
+        data.map((theater) => {
+          return listMoviesByTheater(theater.theater_id).then((array) => {
+            const results = {
+              ...theater,
+              movies: array,
+            };
+
+            return results;
+          });
+        })
+      )
+    );
 }
 
 module.exports = {
